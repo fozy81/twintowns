313f28da1a43bd9effa8fcbc1639939b788ef831
Tim Foster <fozy81@hotmail.com>
2014-11-15 11:59:08 +0000

    initial

---
+++ wikiscrraper.R
@@ -0,0 +1,12 @@
+library(XML)
+library(RCurl)
+
+path <-"http://en.wikipedia.org/wiki/List_of_twin_towns_and_sister_cities_in_the_United_Kingdom#Conwy"
+webpage <- getURL(path)
+webpage <- readLines(tc <- textConnection(webpage)); close(tc)
+
+pagetree <- htmlTreeParse(webpage, error=function(...){}, useInternalNodes = TRUE)
+tablehead <- xpathSApply(pagetree, "//li//", xmlValue)
+tablehead2 <- xpathSApply(pagetree, "//dd", xmlValue)
+tablehead3 <- xpathSApply(pagetree, "//li|//dd", xmlValue)
+class(pagetree)
