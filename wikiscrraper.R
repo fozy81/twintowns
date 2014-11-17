dab5aa6784124c5448274693d2afe53a5f1fa0b7
fozy81 <fozy81@hotmail.com>
2014-11-17 22:41:31 +0000

    developing get coordinates

--- wikiscrraper.R
+++ wikiscrraper.R
@@ -6,8 +6,33 @@ webpage <- getURL(path)
 webpage <- readLines(tc <- textConnection(webpage)); close(tc)
 
 library(stringr)
-matched <- str_match_all(webpage, "<dd><a href=\"(.*?)\"")
-linkslist<- do.call(rbind, lapply(matched, data.frame, stringsAsFactors=FALSE,check.names=F)) 
+#town <- str_match_all(webpage, "<li><a href=\"(.*?)\"") 
+#twins <- str_match_all(webpage, "<dd><a href=\"(.*?)\"")
+all <-  str_match_all(webpage, "<dd><a href=\"(.*?)\"|<li><a href=\"(.*?)\"")
+#town.df <- do.call(rbind, lapply(town, data.frame, stringsAsFactors=FALSE,check.names=F)) 
+#twins.df <- do.call(rbind, lapply(twins, data.frame, stringsAsFactors=FALSE,check.names=F)) 
+all.df <- do.call(rbind, lapply(all, data.frame, stringsAsFactors=FALSE,check.names=F)) 
+all.df$'4' <- paste(all.df$'2', all.df$'3', sep="") # paste all towns into single column
+
+getCoordinates <- lapply(all.df$'4'[1:100], function(x){
+  
+  path <- paste("http://en.wikipedia.org",x,sep="")
+  
+   webpage <- getURL(path)
+  webpage <- readLines(tc <- textConnection(webpage)); close(tc)
+  all <-  str_match_all(webpage, "lat\":(.*?),\"|lon\":(.*?),\"|<title>(.*?)-")
+  all.df <- do.call(rbind, lapply(all, data.frame, stringsAsFactors=FALSE,check.names=F)) 
+ # all.df[1] <- paste(x)
+  Sys.sleep(0.2)
+return(all.df)
+})
+
+coordinates <- do.call(rbind, lapply(getCoordinates, data.frame, stringsAsFactors=FALSE,check.names=F)) 
+coordinates[,3] <- gsub("}","",coordinates[,3] )
+
+,"wgCoordinates":{"lat":49.9,"lon":10.9},
+
+
 links <- matched[[1]][, 1]
 
 pagetree <- htmlTreeParse(webpage, error=function(...){}, useInternalNodes = TRUE)
